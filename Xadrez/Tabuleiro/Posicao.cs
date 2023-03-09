a30e6928ba335b972302f08f389e34041a05dc4f
LUCAS AGUIAR <colinesoft@gmail.com>
2023-03-09 17:02:25 -0300

    Criada a classe de posicao X,Y para o tabuleiro

---
+++ Xadrez/Tabuleiro/Posicao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez.Tabuleiro
+{
+    public class Posicao
+    {
+        public int Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public Posicao(int coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public override string ToString()
+        {
+            return $"{Coluna},{Linha}";
+        }
+    }
+}
